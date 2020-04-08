41e6409469c3e3018d8a76d129b0b64a31c7e2b3
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2020-04-08 07:49:17 +0100

    WIP

--- rsocket-examples/src/main/java/io/rsocket/examples/transport/tcp/channel/ChannelEchoClient.java
+++ rsocket-examples/src/main/java/io/rsocket/examples/transport/tcp/channel/ChannelEchoClient.java
@@ -16,48 +16,54 @@
 
 package io.rsocket.examples.transport.tcp.channel;
 
-import io.rsocket.AbstractRSocket;
-import io.rsocket.ConnectionSetupPayload;
-import io.rsocket.Payload;
-import io.rsocket.RSocket;
-import io.rsocket.RSocketFactory;
-import io.rsocket.SocketAcceptor;
+import io.rsocket.*;
 import io.rsocket.frame.decoder.PayloadDecoder;
-import io.rsocket.transport.local.LocalClientTransport;
-import io.rsocket.transport.local.LocalServerTransport;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.transport.netty.server.TcpServerTransport;
 import io.rsocket.util.ByteBufPayload;
-import java.time.Duration;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
+import java.util.stream.Collectors;
+
 public final class ChannelEchoClient {
-  static final Payload payload1 = ByteBufPayload.create("Hello ");
+
+  private static final Logger logger = LoggerFactory.getLogger(ChannelEchoClient.class);
+
 
   public static void main(String[] args) {
     RSocketFactory.receive()
+        .fragment(64)
         .frameDecoder(PayloadDecoder.ZERO_COPY)
         .acceptor(new SocketAcceptorImpl())
-        .transport(LocalServerTransport.create("localhost"))
+        .transport(TcpServerTransport.create(7000))
         .start()
         .subscribe();
 
     RSocket socket =
         RSocketFactory.connect()
+            .fragment(64)
             .keepAliveAckTimeout(Duration.ofMinutes(10))
             .frameDecoder(PayloadDecoder.ZERO_COPY)
-            .transport(LocalClientTransport.create("localhost"))
+            .transport(TcpClientTransport.create(7000))
             .start()
             .block();
 
-    Flux.range(0, 100000000)
-        .concatMap(i -> socket.fireAndForget(payload1.retain()))
-        //        .doOnNext(p -> {
-        ////            System.out.println(p.getDataUtf8());
-        //            p.release();
-        //        })
-        .blockLast();
+      Flux<Payload> input = Flux.just("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
+              .flatMap(s -> Mono.just(s).repeat(1000).collect(Collectors.joining("", "[", "]")))
+              .doOnNext(logger::debug)
+              .map(ByteBufPayload::create);
+
+      socket.requestChannel(input)
+              .map(Payload::getDataUtf8)
+              .doFinally(signalType -> socket.dispose())
+              .then()
+              .block();
   }
 
   private static class SocketAcceptorImpl implements SocketAcceptor {
